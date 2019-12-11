4b05dc110cfcf60030de233be1d4630da58a0e4c
hjc99 <208418895_hjc@sina.com>
2019-12-11 19:53:24 +0800

    huang

---
+++ kiki/src/k.java
@@ -0,0 +1,11 @@
+/**
+ * @author橙橙
+ * @site
+ * @company xxx公司
+ * @create 2019-12-11 19:51
+ */
+public class k {
+    public static void main(String[] args) {
+        System.out.println("橙");
+    }
+}
